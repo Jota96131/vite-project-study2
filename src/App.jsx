86ad7edc06e10625d7f5be35e96f822599925597
Gushiken Jota <j13119613@gmail.com>
2026-02-05 11:57:04 +0900

    ファイルを追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 
-// Create a single supabase client for interacting with your database
-const supabase = createClient(
-  "https://xyzcompany.supabase.co",
-  "publishable-or-anon-key",
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY,
 );
 
 export const App = () => {
